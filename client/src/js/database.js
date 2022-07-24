8a67a8820fdec6cdf0ec2ecbaa473159ebe2129c
raydover <105178034+raydover@users.noreply.github.com>
2022-07-24 18:49:33 -0400

    commit changes to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,23 +14,24 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const trans = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('jate');
+  const store = trans.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({ id: id, value: value });
+  const request = store.put({ id: id, value: content });
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  result
+    ? console.error('putDb not implemented')
+    : console.log('🚀 - data saved to the database', result.value);
 };
 
 
@@ -42,19 +43,19 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readwrite');
+  const trans = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('jate');
+  const store = trans.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  result
+    ? console.error('getDb not implemented')
+    : console.log('🚀 - data saved to the database', result.value);
 };
-
 
 initdb();
