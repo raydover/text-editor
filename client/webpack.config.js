a0b46c4a3c32781c974076410e8e282237111523
raydover <105178034+raydover@users.noreply.github.com>
2022-07-24 19:18:55 -0400

    edit webpack to match TODOs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,11 +2,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -25,11 +23,12 @@ module.exports = () => {
         template: './index.html',
         title: 'JATE',
       }),
-      // new MiniCssExtractPlugin(),
+      // Service Worker
       new InjectManifest({
         swSrc: './src/sw.js',
         swDest: 'src-sw.js',
       }),
+      // Manifest.json
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
@@ -50,15 +49,12 @@ module.exports = () => {
       })
     ],
 
+    // TODO: Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        // {
-        //   test: /\.css$/i,
-        //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        // },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          test: /\.css$/i,
+          use: ['style.loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
